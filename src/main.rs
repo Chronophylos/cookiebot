a3d8aa1f8548dab375b61f30efa4e9f3475adb33
Chronophylos <nikolai@chronophylos.com>
2020-09-30 13:55:08 +0200

    add self updater

--- src/main.rs
+++ src/main.rs
@@ -502,9 +502,38 @@ fn total_from_captures(captures: Captures) -> Result<u64> {
     Ok(total)
 }
 
+#[cfg(not(debug_assertions))]
+fn update() -> Result<()> {
+    use self_update::{backends::github::Update, cargo_crate_version, Status::*};
+
+    let status = Update::configure()
+        .repo_owner("Chronophylos")
+        .repo_name("cookiebot")
+        .bin_name(env!("CARGO_BIN_NAME"))
+        .show_download_progress(true)
+        .current_version(cargo_crate_version!())
+        .build()?
+        .update()?;
+
+    match status {
+        UpToDate(_) => info!("Version is up to date"),
+        Updated(version) => info!("Updated to {}", version),
+    }
+
+    Ok(())
+}
+
+#[cfg(debug_assertions)]
+fn update() -> Result<()> {
+    info!("Running in dev mode. Skipping version check.");
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
+    update()?;
 
     let user_config = UserConfig::builder()
         .name(&CONFIG.username)
