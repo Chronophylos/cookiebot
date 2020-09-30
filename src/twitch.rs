6919b6e11245655f3cae20db609b92bf4e5d3abd
Chronophylos <nikolai@chronophylos.com>
2020-09-30 14:35:26 +0200

    add context to errors

--- src/twitch.rs
+++ src/twitch.rs
@@ -1,21 +1,27 @@
+use anyhow::{Context, Result};
 use log::info;
 use twitchchat::{connector, AsyncRunner, UserConfig};
 
-pub async fn connect(user_config: &UserConfig, channel: &str) -> anyhow::Result<AsyncRunner> {
+pub async fn connect(user_config: &UserConfig, channel: &str) -> Result<AsyncRunner> {
     // create a connector using ``smol``, this connects to Twitch.
     // you can provide a different address with `custom`
     // this can fail if DNS resolution cannot happen
-    let connector = connector::SmolConnectorTls::twitch()?;
+    let connector =
+        connector::SmolConnectorTls::twitch().context("Creating connector to twitch")?;
 
-    info!("Connecting to twitch");
+    info!("Connecting to twitch chat server");
     // create a new runner. this is a provided async 'main loop'
     // this method will block until you're ready
-    let mut runner = AsyncRunner::connect(connector, user_config).await?;
+    let mut runner = AsyncRunner::connect(connector, user_config)
+        .await
+        .context("Connecting to twitch")?;
 
-    info!("Connected with Identity: {:#?}", runner.identity);
+    info!("Connected as: {}", runner.identity.username());
 
-    info!("Attempting to join '{}'", channel);
-    runner.join(channel).await?;
+    runner
+        .join(channel)
+        .await
+        .with_context(|| format!("Joining channel {}", channel))?;
     info!("Joined '{}'!", channel);
 
     Ok(runner)
