d2f20e0be7699a2b7a5dcf0af82016a0a86550ef
Chronophylos <nikolai@chronophylos.com>
2021-04-09 12:34:58 +0200

    remove self-updater

--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,7 @@
 use anyhow::{Context, Result};
 use clap::{App, Arg};
 use cookiebot::{Config, ThePositiveBotBot};
-use log::{error, info};
 use metrics_exporter_prometheus::PrometheusBuilder;
-
-#[cfg(not(debug_assertions))]
-fn update() -> Result<()> {
-    use self_update::{backends::github::Update, cargo_crate_version, Status::*};
-
-    let status = Update::configure()
-        .repo_owner("Chronophylos")
-        .repo_name("cookiebot")
-        .bin_name(env!("CARGO_BIN_NAME"))
-        .show_download_progress(true)
-        .current_version(cargo_crate_version!())
-        .build()?
-        .update()?;
-
-    match status {
-        UpToDate(_) => info!("Version is up to date"),
-        Updated(version) => {
-            info!("Updated to {}", version);
-            use std::process::{exit, Command};
-
-            let code = Command::new(std::env::args().next().expect("executable path"))
-                .spawn()?
-                .wait()?;
-
-            if code.success() == false {
-                exit(code.code().unwrap_or(1));
-            } else {
-                exit(0);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-#[cfg(debug_assertions)]
-fn update() -> Result<()> {
-    info!("Running in dev mode. Skipping version check.");
-
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -77,10 +35,6 @@ async fn main() -> Result<()> {
     let config = Config::from_path(config_path)?;
 
     let accept_invalid_certs = matches.is_present("accept-invalid-certs");
-
-    if let Err(err) = update() {
-        error!("Could not update binary: {}", err);
-    }
 
     ThePositiveBotBot::new(
         config.username,
