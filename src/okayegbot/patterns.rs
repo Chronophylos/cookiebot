78dfe923a7806a743d306f32162f3f2edda8520b
Chronophylos <nikolai@chronophylos.com>
2021-04-12 09:27:32 +0200

    actually fix the bug

--- src/okayegbot/patterns.rs
+++ src/okayegbot/patterns.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 lazy_static! {
     // https://regex101.com/r/6gc79V/3
     #[derive(Debug)]
-    pub static ref CLAIM_GOOD: Regex = Regex::new(r#"@(?P<username>\w+) \| [^\|]* \| (?P<amount>[+-]\d+) egs \| Total egs: (?P<total>\d+) 🥚"#).unwrap();
+    pub static ref CLAIM_GOOD: Regex = Regex::new(r#"@(?P<username>\w+) \| [^\|]* \| (?P<amount>[+-]\d+) +egs \| Total egs: (?P<total>\d+) 🥚"#).unwrap();
 
     // https://regex101.com/r/g4FpOL/1/
     #[derive(Debug)]
@@ -46,6 +46,30 @@ mod test {
     #[test]
     fn claim_good2() {
         let text = "@chronophylos | is this a YOLK? nam1Okayeg | +1 egs | Total egs: 92 🥚 ";
+        let captures = CLAIM_GOOD.captures(text).expect("regex should match");
+
+        dbg!(&captures);
+
+        assert_eq!(
+            captures.name("username").unwrap().as_str(),
+            "chronophylos",
+            "wrong username"
+        );
+        assert_eq!(
+            captures.name("amount").unwrap().as_str(),
+            "+1",
+            "wrong eg changed amount"
+        );
+        assert_eq!(
+            captures.name("total").unwrap().as_str(),
+            "92",
+            "wrong total eg amount"
+        );
+    }
+
+    #[test]
+    fn claim_good_with_weird_space() {
+        let text = "@chronophylos | is this a YOLK? nam1Okayeg | +1  egs | Total egs: 92 🥚 ";
         let captures = CLAIM_GOOD.captures(text).expect("regex should match");
 
         dbg!(&captures);
